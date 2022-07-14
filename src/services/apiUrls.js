8cc0529f67418795caba1dd9a7e97c17a884745b
manish13k <manish13ksangwan@gmail.com>
2022-07-14 22:23:12 +0530

    Delete src directory

--- src/services/apiUrls.js
+++
@@ -1,5 +0,0 @@
-export const UserAPI = 'https://jsonplaceholder.typicode.com/users';
-
-export const PhotoAPI = 'https://jsonplaceholder.typicode.com/photos';
-
-export const AlbumAPI = 'https://jsonplaceholder.typicode.com/albums';
